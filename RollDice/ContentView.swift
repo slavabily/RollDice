676966e5b2d9700598547ac97e54141828d228e1
slavabily <slavabily@me.com>
2020-07-05 18:24:40 +0300

    minimum functionality

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -9,8 +9,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rolledNumber = Int()
+    @State private var score = 0
+    
+    var diceTypes = ["4", "6"]
+    @State private var diceTypeSelection = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            NavigationView {
+                
+                VStack {
+                    Form {
+                        Section {
+                            Picker(selection: $diceTypeSelection, label: Text("Select the dice type")) {
+                                ForEach(0..<diceTypes.count) {
+                                    Text(self.diceTypes[$0])
+                                }
+                            }.pickerStyle(SegmentedPickerStyle())
+                        }
+                        
+                        Section {
+                            Button("Roll") {
+                                self.randomNumber()
+                            }
+                            .font(.largeTitle)
+                            
+                            Text("Number: \(rolledNumber != 0 ? "\(rolledNumber)" : "")")
+                                .padding()
+                        }
+                    }
+                   
+                    
+                    
+                }
+                .navigationBarTitle("Roll the Dice")
+            }
+            .tabItem {
+                Image(systemName: "hexagon.fill")
+                Text("Roll Dice")
+            }
+            .tag(0)
+            
+            NavigationView {
+                VStack {
+                    Text("Score: \(score)")
+                        .font(.largeTitle)
+                }
+                .navigationBarTitle("Results")
+            }
+            .tabItem {
+                Image(systemName: "dollarsign.circle.fill")
+                Text("Results")
+            }
+            .tag(1)
+        }
+    }
+    
+    func randomNumber() {
+        let number = Int.random(in: 1...6)
+        
+        rolledNumber = number
+        
+        score += number
     }
 }
 
